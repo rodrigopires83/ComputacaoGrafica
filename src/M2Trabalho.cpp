5ee7ce6a4d554eea85af311c88c85e1fff7758bb
rodrigopires83 <102845202+rodrigopires83@users.noreply.github.com>
2025-06-22 18:14:09 -0300

    Add files via upload

--- src/M2Trabalho.cpp
+++ src/M2Trabalho.cpp
@@ -1,10 +1,8 @@
-/* Hello Triangle - código adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
- *
- * Adaptado por Rossana Baptista Queiroz
- * para as disciplinas de Processamento Gráfico/Computação Gráfica - Unisinos
- * Versão inicial: 7/4/2017
- * Última atualização em 07/03/2025
- */
+/*	Aluno: Rodrigo Ferreira Pires
+	Disciplina: Computação Gráfica
+	Professora: Rossana B Queiroz
+
+*/
 
 #include <iostream>
 #include <string>
